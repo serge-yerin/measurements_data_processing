ee63a344602cd3102e1f20f2a97010f52f3f62ad
Serge Yerin <serge-yerin@users.noreply.github.com>
2019-02-22 23:38:55 +0200

    make the script work for the first time

--- script_LVM_viewer.py
+++ script_LVM_viewer.py
@@ -6,7 +6,7 @@
 #*************************************************************
 
 FilesOrFolder = 1      # To analyze particular files (0) or all files in folder (1)
-directory = 'DATA'
+directory = 'DATA/'
 make_phase_linear = 1  # For phase data make linearization instead of jumps from -180 to 180
 filename = []
 
@@ -21,48 +21,23 @@ import pylab
 import time
 import os
 
+from f_text_manipulations import find_between, float_convert
+from f_phase_data_linearization import phase_linearization
+
+
 #*************************************************************
 #                       FUNCTIONS                            *
 #*************************************************************
 
-# Searching of a line part between quotes (or any other symbols)
-def find_between(string, start, stop):
-    try:
-        begin = string.index(start) + len(start)
-        end = string.index(stop, begin)
-        return string[begin:end]
-    except ValueError:
-        return "Error!"
-
-# Conversion string to float with replacing come to point    
-def float_convert (string):
-    try:
-        floatnum = float(string.replace(',','.'))
-    except:
-        print ("Error converting to float!")
-        floatnum = 0
-        quit()
-    return floatnum    
-    
-# Linearization of data     
-def phase_linearization(matrix):
-    const = 0
-    matrix_lin = np.zeros((len(matrix)))
-    matrix_lin[:] = matrix[:]
-    for elem in range(1, len(matrix)):
-        if (matrix[elem] - matrix[elem-1]) > 300:
-            const = const + 360
-        matrix_lin[elem] = matrix_lin[elem] - const
-    return matrix_lin
 
 #*************************************************************
 #                       MAIN PROGRAM                         *
 #*************************************************************        
 
 for i in range (0,5) : print (' ')
-print ('   ****************************************************')
-print ('   *         Obzor-103 data file viewer  v1.0         *      (c) YeS 2018')
-print ('   ****************************************************')
+print ('   *****************************************************************')
+print ('   *  Ground parameters measurement system data file viewer  v1.0  *      (c) YeS 2019')
+print ('   *****************************************************************')
 for i in range (0,2) : print (' ')
 
 currentTime = time.strftime("%H:%M:%S")
@@ -72,7 +47,7 @@ print (' ')
 
 # *** Creating a folder where all pictures and results will be stored (if is doen't exist) ***
 
-newpath = "SMPviewer Results" 
+newpath = 'LVM_viewer_results' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
     
@@ -124,40 +99,103 @@ for file in range (len(filenamelist)):   # Loop by files in list
         if line_no > 22 and len(line.split()) > 0:
             no_of_counts = no_of_counts + 1
             ADC_counts.append(float(line.split()[0].replace(',','.')))
-            ADC_counts.append(float(line.split()[1].replace(',','.')))
-    print (no_of_counts)
-
+            ADC_values.append(float(line.split()[1].replace(',','.')))
+    print ('   * Number of points: ', no_of_counts)
 
     
     # *** Rearranging data to single array ***        
     array_ADC_counts = np.append(array_ADC_counts, ADC_counts, axis=0)
     array_ADC_values = np.append(array_ADC_values, ADC_values, axis=0)
     
- 
+# Reshaping array as (counts * files)
 array_ADC_counts = np.reshape(array_ADC_counts, [no_of_counts, len(filenamelist)], order='F')
+array_ADC_values = np.reshape(array_ADC_values, [no_of_counts, len(filenamelist)], order='F')
     
 del ADC_counts, ADC_values
 
 
 
-print (array_ADC_counts.shape)
-print (array_ADC_values.shape)
+#print (array_ADC_counts.shape)
+#print (array_ADC_values.shape)
 
 
+
+
+print ('\n\n    Building figures... \n ')
+
+
+
+
+# *** Plotting the graphs ***
+
+for i in range (len(filenamelist)):
+    plt.figure()
+    plt.plot(array_ADC_counts[:, i], array_ADC_values[:, i], linewidth = '1.50', label = filename[i])
+    plt.ylim((-0.05, 1.5))
+    plt.xlabel('Points in time')
+    plt.ylabel('Amplitude ratio')
+    plt.suptitle('Ground parameters measurements', fontsize = 12, fontweight = 'bold')
+    #plt.title( 'Measured: ' + fileDate + ' at ' + fileTime, fontsize = 8)
+    plt.grid(b = True, which = 'both', color = '0.65', linestyle = '--')
+    plt.legend(loc = 'upper right', fontsize = 5)
+    plt.text(0.73, 0.02,'Processed '+currentDate+ ' at '+currentTime, fontsize = 5, transform = plt.gcf().transFigure)
+    pylab.savefig('LVM_viewer_results/' + filename[i] + '.png', bbox_inches='tight', dpi = 200)
+    #plt.show()
+    plt.close('all')
+
+
+# *** Plotting the graphs ***
+
+
+
+plt.figure()
+for i in range(len(filenamelist)):
+    plt.plot(array_ADC_counts[:, i], array_ADC_values[:, i], linewidth = '1.50', label = filename[i])
+
+plt.ylim((-0.05, 1.5))
+plt.xlabel('Points in time')
+plt.ylabel('Amplitude ratio')
+plt.suptitle('Ground parameters measurements', fontsize = 12, fontweight = 'bold')
+#plt.title( 'Measured: ' + fileDate + ' at ' + fileTime, fontsize = 8)
+plt.grid(b = True, which = 'both', color = '0.65', linestyle = '--')
+plt.legend(loc='center left', fontsize = 5, bbox_to_anchor=(1, 0.5))
+plt.text(0.73, 0.02,'Processed '+currentDate+ ' at '+currentTime, fontsize = 5, transform = plt.gcf().transFigure)
+pylab.savefig('LVM_viewer_results/01_' + filename[0] + '_' + filename[len(filenamelist)-1] +'.png', bbox_inches='tight', dpi = 200)
+#plt.show()
+plt.close('all')
+
+
+
+points_to_plot = [1800, 3000, 7500, 10000, 13000, 16000]
+j = 0
+
+plt.figure()
+plt.plot(filename[:], array_ADC_values[points_to_plot[0], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[0]))
+plt.plot(filename[:], array_ADC_values[points_to_plot[1], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[1]))
+plt.plot(filename[:], array_ADC_values[points_to_plot[2], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[2]))
+plt.plot(filename[:], array_ADC_values[points_to_plot[3], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[3]))
+plt.plot(filename[:], array_ADC_values[points_to_plot[4], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[4]))
+plt.plot(filename[:], array_ADC_values[points_to_plot[5], :], linewidth = '1.50', label = 'Point = ' + str(points_to_plot[5]))
+
+plt.ylim((-0.05, 1.5))
+plt.xlabel('Points in time')
+plt.ylabel('Amplitude ratio')
+plt.suptitle('Ground parameters measurements', fontsize = 12, fontweight = 'bold')
+#plt.title( 'Measured: ' + fileDate + ' at ' + fileTime, fontsize = 8)
+plt.grid(b = True, which = 'both', color = '0.65', linestyle = '--')
+plt.legend(loc = 'upper right', fontsize = 5)
+plt.text(0.73, 0.02,'Processed '+currentDate+ ' at '+currentTime, fontsize = 5, transform = plt.gcf().transFigure)
+pylab.savefig('LVM_viewer_results/00_' +str(points_to_plot[j]) + '_' + filename[0] + '_' + filename[len(filenamelist)-1] +'.png', bbox_inches='tight', dpi = 200)
+
+#plt.show()
+plt.close('all')
 
 
 
 '''
-# *** Printing information about data ***        
 
-print (' \n\n Parameters of file: \n ')
-print ('      Number of frequency steps in file is', Num_of_steps)
-print ('\n      Measured parameters:')
-for i in range (2*setNo):   
-    print ('      * ', param_names[i])
-print (' \n\n ')
 
-print ('\n\n    Building figures... \n ')
+
 
 
 # *** Plotting the graphs ***
