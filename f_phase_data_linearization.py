c9f7227e8a86e173cd35793d49a709cc3975fcb0
Serge Yerin <serge-yerin@users.noreply.github.com>
2020-01-21 15:31:56 +0200

    program is ready for simple use

--- f_phase_data_linearization.py
+++ f_phase_data_linearization.py
@@ -8,11 +8,16 @@ def phase_linearization(matrix):
     '''
     Makes a vector of phase values linear without 360 deg subtraction
     '''
-    const = 0
     matrix_lin = np.zeros((len(matrix)))
     matrix_lin[:] = matrix[:]
+    const = 0
     for elem in range(1, len(matrix)):
         if (matrix[elem] - matrix[elem-1]) > 300:
             const = const + 360
         matrix_lin[elem] = matrix_lin[elem] - const
+    const = 0
+    for elem in range(1, len(matrix)):
+        if (matrix[elem-1] - matrix[elem]) > 300:
+            const = const + 360
+        matrix_lin[elem] = matrix_lin[elem] + const
     return matrix_lin
