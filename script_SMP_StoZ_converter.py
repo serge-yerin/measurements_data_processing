f7b407b16ce0968133daf966002c17acaacc13c4
gurt <yerin.serge@gmail.com>
2021-07-16 10:05:38 +0300

    added another format of input data and updated code style

--- script_SMP_StoZ_converter.py
+++ script_SMP_StoZ_converter.py
@@ -1,19 +1,18 @@
 # Python 3
 # Program intended to analyze data from measurements with OBZOR-103
 
-#*************************************************************
-#                        PARAMETERS                          *
-#*************************************************************
+# *************************************************************
+#                         PARAMETERS                          *
+# *************************************************************
 
 FilesOrFolder = 1      # To analyze particular files (0) or all files in folder (1)
 directory = 'DATA/'
 make_phase_linear = 1  # For phase data make linearization instead of jumps from -180 to 180
 filename = []
 
-
-#*************************************************************
-#                   IMPORT LIBRARIES                         *
-#*************************************************************
+# *************************************************************
+#                    IMPORT LIBRARIES                         *
+# *************************************************************
 
 import matplotlib.pyplot as plt   # Importing MATPLOTLIB library 
 import numpy as np
@@ -24,23 +23,17 @@ import os
 from f_text_manipulations import float_convert, find_between
 from f_phase_data_linearization import phase_linearization
 
- 
+# *************************************************************
+#                        MAIN PROGRAM                         *
+# *************************************************************
 
-
-#*************************************************************
-#                       MAIN PROGRAM                         *
-#*************************************************************        
-
-for i in range (0,5) : print (' ')
-print ('   ****************************************************')
-print ('   *         Obzor-103 data file viewer  v1.0         *      (c) YeS 2018')
-print ('   ****************************************************')
-for i in range (0,2) : print (' ')
+print('\n\n\n\n\n   ****************************************************')
+print('   *         Obzor-103 data file viewer  v1.0         *      (c) YeS 2018')
+print('   **************************************************** \n\n')
 
 currentTime = time.strftime("%H:%M:%S")
 currentDate = time.strftime("%d.%m.%Y")
-print ('  Today is ', currentDate, ' time is ', currentTime)
-print (' ')
+print('  Today is ', currentDate, ' time is ', currentTime, '\n')
 
 # *** Creating a folder where all pictures and results will be stored (if is doen't exist) ***
 
@@ -55,15 +48,15 @@ if FilesOrFolder == 1:
     
     # *** Search SMP files in the directory ***
     filename = []
-    file_name_list=[]
+    file_name_list = []
     i = 0
-    print ('  Directory: ', directory, '\n')
-    print ('  List of files to be analyzed: ')
+    print('  Directory: ', directory, '\n')
+    print('  List of files to be analyzed: ')
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.smp'):
                 i = i + 1
-                print ('         ', i, ') ', file)
+                print('         ', i, ') ', file)
                 filename.append(str(file))
                 file_name_list.append(str(os.path.join(root, file)))
 else:
@@ -73,15 +66,15 @@ else:
 # *** Reading data from files ***
 
 parameters = []
-for file in range (len(file_name_list)):   # Loop by files in list
+for file in range(len(file_name_list)):   # Loop by files in list
 
     # *** Opening datafile ***
     fname = file_name_list[file]
     handle = open(fname, 'r')
     
-    print ('\n\n Analyzing file:', filename[file], ' \n')
+    print('\n\n Analyzing file:', filename[file], ' \n')
     
-    #   *** Reading datafile and filling the matices of values ***
+    #   *** Reading datafile and filling the matrices of values ***
     frequency = []
     list_01 = []
     list_02 = []
@@ -91,11 +84,10 @@ for file in range (len(file_name_list)):   # Loop by files in list
     param_names_01 = []
     param_names_02 = []
     
-    
     for line in handle:                        # Loop by all lines in file
-        if line.startswith(";") :              # Searching comments
-            print (line.rstrip())
-            if line.startswith("; Obzor-102 Network analyzer data file.") : 
+        if line.startswith(";"):              # Searching comments
+            print(line.rstrip())
+            if line.startswith("; Obzor-102 Network analyzer data file."):
                 words_in_line = line.split()
                 fileDate = words_in_line[6]    # reading date and time of file creation
                 fileTime = words_in_line[7]
@@ -108,14 +100,18 @@ for file in range (len(file_name_list)):   # Loop by files in list
                 param_names_01.append(find_between(line, '"', '"'))
             if line.startswith("; Parameter2="):
                 param_names_02.append(find_between(line, '"', '"'))
-            else: continue
+            else:
+                continue
         else:
             for column in range(0, 3):
-                num = line[0 + column*14 : 14 + column*14]
+                num = line[0 + column*14: 14 + column*14]
                 floatnum = float_convert (num)
-                if (column == 0 and setNo == 1): frequency.append(floatnum)
-                if column == 1: list_01.append(floatnum)
-                if column == 2: list_02.append(floatnum)
+                if column == 0 and setNo == 1:
+                    frequency.append(floatnum)
+                if column == 1:
+                    list_01.append(floatnum)
+                if column == 2:
+                    list_02.append(floatnum)
             freqSteps = freqSteps + 1
     
     # *** Rearranging data to single array ***        
@@ -131,21 +127,20 @@ del param_names_01, param_names_02, list_01, list_02
 
 # *** Printing information about data ***        
 
-print (' \n\n Parameters of file: \n ')
-print ('      Number of frequency steps in file is', Num_of_steps)
-print ('\n      Measured parameters:')
-for i in range (2*setNo):   
-    print ('      * ', param_names[i])
-print (' \n\n ')
-
-print ('\n\n    Building figures... \n ')
+print(' \n\n Parameters of file: \n ')
+print('      Number of frequency steps in file is', Num_of_steps)
+print('\n      Measured parameters:')
+for i in range(2 * setNo):
+    print('      * ', param_names[i])
+print('\n\n\n\n    Building figures... \n ')
 
 
 # *** Plotting the graphs ***
 
 if len(param_names) > 2:
 
-    if param_names[0] == 'S11 Real' and param_names[2] == 'S11 Imag' and param_names[1] == 'S21 Real' and param_names[3] == 'S21 Imag':
+    if param_names[0] == 'S11 Real' and param_names[2] == 'S11 Imag' and \
+            param_names[1] == 'S21 Real' and param_names[3] == 'S21 Imag':
 
         files_no = len(file_name_list)
         frequency_matrix = []
@@ -177,6 +172,36 @@ if len(param_names) > 2:
             full_data[file * len(labels) + 10][:] = phase_linearization(np.angle(S_21, deg=True)[:])
             impedance[file][:] = ((1 + S_11[:]) / (1 - S_11[:]))
 
+
+if len(param_names) > 2:
+
+    if param_names[0] == 'S11 Real' and param_names[1] == 'S11 Ampl Log' and \
+            param_names[2] == 'S11 SWR' and param_names[3] == 'S11 Imag'and \
+            param_names[4] == 'S11 Phase' and param_names[5] == 'S11 SWR':
+
+        files_no = len(file_name_list)
+        frequency_matrix = []
+        labels = ['S11 Amplitude', 'S11 Power', 'S11, dB', 'S11 Phase, deg', 'S11 Phase linear, deg', 'S11 VSWR']
+
+        data_length = len(frequency)
+
+        S_11 = np.zeros((data_length), dtype=complex)
+        full_data = np.zeros((len(labels) * files_no, data_length))
+        impedance = np.zeros((files_no, data_length), dtype=complex)
+
+        for file in range(files_no):
+
+            S_11[:] = parameters[:, 0, file] + 1j * parameters[:, 3, file]
+
+            full_data[file * len(labels) + 0][:] = np.abs(S_11)[:]
+            full_data[file * len(labels) + 1][:] = np.power(np.abs(S_11), 2)[:]
+            full_data[file * len(labels) + 2][:] = 10 * np.log10(np.power(np.abs(S_11), 2))[:]
+            full_data[file * len(labels) + 3][:] = np.angle(S_11, deg=True)[:]
+            full_data[file * len(labels) + 4][:] = phase_linearization(np.angle(S_11, deg=True)[:])
+            full_data[file * len(labels) + 5][:] = (1 + np.abs(S_11)[:]) / (1 - np.abs(S_11)[:])
+            impedance[file][:] = ((1 + S_11[:]) / (1 - S_11[:]))
+
+
 if len(param_names) == 2:
     if param_names[0] == 'S11 Real' and param_names[1] == 'S11 Imag':
 
@@ -187,13 +212,11 @@ if len(param_names) == 2:
         data_length = len(frequency)
 
         S_11 = np.zeros((data_length), dtype=complex)
-        #S_21 = np.zeros((data_length), dtype=complex)
         full_data = np.zeros((len(labels) * files_no, data_length))
         impedance = np.zeros((files_no, data_length), dtype=complex)
 
         for file in range(files_no):
             S_11[:] = parameters[:, 0, file] + 1j * parameters[:, 1, file]
-            #S_21[:] = parameters[:, 1, file] + 1j * parameters[:, 3, file]
 
             full_data[file * len(labels) + 0][:] = np.abs(S_11)[:]
             full_data[file * len(labels) + 1][:] = np.power(np.abs(S_11), 2)[:]
@@ -229,11 +252,13 @@ fig.suptitle('Measurement results', fontsize=12, fontweight='bold')
 ax1.grid(b=True, which='both', color='0.65', linestyle='--')
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 for file in range(files_no):
-    ax2.plot(frequency, np.imag(impedance[file])[:] * 50, linewidth='1.50', linestyle='--', label='X '+file_name_list[file])
+    ax2.plot(frequency, np.imag(impedance[file])[:] * 50, linewidth='1.50',
+             linestyle='--', label='X ' + file_name_list[file])
 ax2.set_ylabel('X input')
 fig.legend(loc='center left', fontsize=5, bbox_to_anchor=(1.05, 0.5))
 fig.text(0.73, 0.02, 'Processed ' + currentDate + ' at ' + currentTime, fontsize=5, transform=plt.gcf().transFigure)
-pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input impedance.png', bbox_inches='tight', dpi=200)
+pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input impedance.png',
+              bbox_inches='tight', dpi=200)
 plt.close('all')
 
 
@@ -249,14 +274,15 @@ fig.suptitle('Measurement results', fontsize=12, fontweight='bold')
 ax1.grid(b=True, which='both', color='0.65', linestyle='--')
 fig.legend(loc='center left', fontsize=5, bbox_to_anchor=(1.05, 0.5))
 fig.text(0.73, 0.02, 'Processed ' + currentDate + ' at ' + currentTime, fontsize=5, transform=plt.gcf().transFigure)
-pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input resistance.png', bbox_inches='tight', dpi=200)
+pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input resistance.png',
+              bbox_inches='tight', dpi=200)
 plt.close('all')
 
 # Plot of input reactance
 fig, ax1 = plt.subplots()
 for file in range(files_no):
     ax1.plot(frequency, np.imag(impedance[file])[:] * 50, linewidth='1.50', label='R '+file_name_list[file])
-#ax1.set_yscale('log')
+# ax1.set_yscale('log')
 ax1.set_ylim(-2500, 500)
 ax1.set_xlabel('Frequency, MHz')
 ax1.set_ylabel('X input')
@@ -264,10 +290,9 @@ fig.suptitle('Measurement results', fontsize=12, fontweight='bold')
 ax1.grid(b=True, which='both', color='0.65', linestyle='--')
 fig.legend(loc='center left', fontsize=5, bbox_to_anchor=(1.05, 0.5))
 fig.text(0.73, 0.02, 'Processed ' + currentDate + ' at ' + currentTime, fontsize=5, transform=plt.gcf().transFigure)
-pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input reactance.png', bbox_inches='tight', dpi=200)
+pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels))) + '_Input reactance.png',
+              bbox_inches='tight', dpi=200)
 plt.close('all')
-
-
 
 
 # Plot of input impedance module
@@ -280,8 +305,9 @@ fig.suptitle('Measurement results', fontsize=12, fontweight='bold')
 ax1.grid(b=True, which='both', color='0.65', linestyle='--')
 fig.legend(loc='center left', fontsize=5, bbox_to_anchor=(0.9, 0.5))
 fig.text(0.73, 0.02, 'Processed ' + currentDate + ' at ' + currentTime, fontsize=5, transform=plt.gcf().transFigure)
-pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels)+1)) + '_Input impedance module.png', bbox_inches='tight', dpi=200)
+pylab.savefig(newpath + '/' + ''.join("{:02.0f}".format(len(labels)+1)) + '_Input impedance module.png',
+              bbox_inches='tight', dpi=200)
 plt.close('all')
 
 
-print ('\n\n    *** Program finished ***   \n\n\n')
+print('\n\n    *** Program finished ***   \n\n\n')
